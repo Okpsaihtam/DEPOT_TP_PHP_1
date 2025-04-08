11e0065c37dbb03ef6dd51dbd0fdaf053eb88f6b
Mathias Celle <[mathias.celle42230@gmail.com]>
2025-04-08 09:40:29 +0200

    projet php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+print ("EXERCICE 1 </br> </br>");
+for ($i = 1; $i <= 10; $i++) {
+    echo $i;
+    echo "</br>";
+}
+
+echo "</br>";
+echo "</br>";
+
+print ("EXERCICE 2 </br> </br>");
+$somme = 0;
+for ($i = 1; $i <= 100; $i++) {
+    $somme = $somme + $i;
+}
+echo $somme;
+echo "</br>";
+
+echo "</br>";
+echo "</br>";
+
+print ("EXERCICE 3 </br> </br>");
+for ($i = 1; $i <= 10; $i++) {
+    echo "5 X $i = ",$i*5, "</br>";
+}
