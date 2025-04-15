185839f271ef63fe3fc18a0a9f3bae09e7ac2cd9
Mathias Celle <[mathias.celle42230@gmail.com]>
2025-04-15 10:57:17 +0200

    Rajout d'une lamborghini

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ class Vehicule {
     }
 }
 
-<<<<<<< HEAD
 class Voiture extends Vehicule {
     private $nombrePortes;
     private $typeCarburant;
@@ -36,7 +35,7 @@ class Voiture extends Vehicule {
     }
     
     public function klaxonner() {
-        return "La voiture klaxonne : Bip Bip !";
+        return "La voiture klaxonne : Attention Attention un pickpocket !";
     }
 }
 
@@ -54,7 +53,7 @@ class Moto extends Vehicule {
     }
 
     public function faireRugir() {
-        return "La moto rugit : Vroooom !";
+        return "La moto rugit : arrrrrrrrrrrrrah !";
     }
 }
 
@@ -69,73 +68,10 @@ $moto = new Moto("Yamaha", "MT-07", 2022, 689);
 echo $moto->getInfos() . "<br>";
 echo $moto->demarrer() . "<br>";
 echo $moto->faireRugir() . "<br>";
-=======
-class vehicule {
-    protected $marque;
-    protected $modele;
-    protected $annee;
 
-    public function __construct($marque, $modele, $annee) {
-        $this->marque = $marque;
-        $this->modele = $modele;
-        $this->annee = $annee;
-    }
+echo "<br>";
 
-    public function getInfo () {
-        return "Marque: $this->marque, Modèle: $this->modele, Année: $this->annee";
-    }
-
-    public function demarrer () {
-        return "Le véhicule démarre.";
-    }
-}
-
-class voiture extends vehicule {
-    private $nombrePortes;
-    private $typeCarburant;
-
-    public function __construct($marque, $modele, $annee, $nombrePortes, $typeCarburant) {
-        parent::__construct($marque, $modele, $annee);
-        $this->nombrePortes = $nombrePortes;
-        $this->typeCarburant = $typeCarburant;
-    }
-
-    public function getInfos() {
-        return parent::getInfo() . ", Portes: $this->nombrePortes, Carburant: $this->typeCarburant";
-    }
-
-    public function klaxonner() {
-        return "La voiture klaxonne : Bip Bip !";
-    }
-}
-
-class Moto extends Vehicule {
-    private $cylindree;
-
-    public function __construct($marque, $modele, $annee, $cylindree) {
-        parent::__construct($marque, $modele, $annee);
-        $this->cylindree = $cylindree;
-    }
-
-    public function getInfo() {
-        return parent::getInfo() . ", Cylindrée: $this->cylindree cc";
-    }
-
-    public function cabrer() {
-        return "La moto cabre sur une roue !";
-    }
-}
-
-$voiture = new Voiture ("Renault", "Zoé", 2021, 5, "électrique");
-echo $voiture->getInfo() . "</br>";
-echo $voiture->demarrer() . "</br>";
-echo $voiture->klaxonner() . "</br></br>";
-
-$moto = new Moto ("Yamaha", "MT-07", 2022, 689);
-echo $moto->getInfo() . "</br>";
-echo $moto->demarrer() . "</br>";
-echo $moto->cabrer() . "</br>";
-
->>>>>>> bf2159f6bc241b14bd57d99f4bfd03010f0ef984
-
-
+$voiture2 = new Voiture("Lamborghini", "Aventador", 2024, 5, "Essence");
+echo $voiture2->getInfos() . "</br>";
+echo $voiture2->demarrer() . "</br>";
+echo $voiture2->klaxonner() . "</br>";
